78268a2d4d3970aca1e06f4ab4f3215c64f5bab7
dmunozbarras <32308848+dmunozbarras@users.noreply.github.com>
2017-10-13 18:48:14 +0200

    Update Eje2-3.py

--- Eje2-3.py
+++ Eje2-3.py
@@ -1,4 +1,4 @@
 km = int(input("Cuantos kilometros a recorrido:"))
 horas = int(input("Cuantas horas a tardado:"))
 km_horas = int(km//horas)
-print"Usted iba a %d km/h:" % (km_horas)
+print"Usted iba a %d km/h" % (km_horas)
